b0d595e975e72f1a727dd4e59632bd4d0e835479
Alexey <falconaleksey@mail.ru>
2022-11-14 10:58:48 +0400

    create file with data from json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {Test1Component} from "./test1/test1.component";
+import { Test1Component } from './test1/test1.component';
+import { Test3Component } from './test3/test3.component';
+import { Test2Component } from './user/components/test2/test2.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    Test1Component
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, Test1Component, Test3Component, Test2Component],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
